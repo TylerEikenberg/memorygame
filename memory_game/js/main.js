b758d0337dc9c80082ca26791e4b3efd1acf4e88
Tyler <Tyler@Scotts-MacBook-Pro-2.local>
2019-09-17 13:47:31 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cards = ["Queen", "Queen", "King", "King"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again!")
+	}
+}
+
+
+console.log("The user flipped " + cardsInPlay[0] + ".");
+console.log("The user flipped " + cardsInPlay[1] + ".");
