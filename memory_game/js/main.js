75b7636e873e173f99ccc0e06694301ffc6e841e
Tyler <Tyler@Scotts-MacBook-Pro-2.local>
2019-09-18 11:44:35 -0400

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,26 @@
+/*
+JS Code for the Memory Game
+Created by Tyler Eikenberg
+*/
 var cards = ["Queen", "Queen", "King", "King"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
+//Define checkForMatch function
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+}
+else {
+alert("Sorry, try again!")
+}
+}
+//Define flipCard function
+function flipCard(cardID){
+console.log("User flipped " + cards[cardID] + ".");
+cardsInPlay.push(cards[cardID]);
 if(cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again!")
-	}
+	checkForMatch();
 }
 
-
-console.log("The user flipped " + cardsInPlay[0] + ".");
-console.log("The user flipped " + cardsInPlay[1] + ".");
+}
+flipCard(0);
+flipCard(2);
